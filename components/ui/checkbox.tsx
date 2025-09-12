24f41a350da785ec1727a74479a2cc239d21bea0
jrippy-cmd <jrippy@localhowl.com>
2025-09-12 15:56:44 -0500

    Create checkbox.tsx

---
+++ components/ui/checkbox.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+import { cn } from "@/utils/cn";
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "peer inline-flex size-18 items-center justify-center rounded-4",
+      "border border-border bg-background text-foreground",
+      "hover:bg-black-alpha-4",
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "disabled:cursor-not-allowed disabled:opacity-50",
+      "data-[state=checked]:border-heat-100 data-[state=checked]:bg-heat-100",
+      className
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className="grid place-items-center text-accent-white">
+      <Check className="size-14" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
