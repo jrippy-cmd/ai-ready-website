d22d622f5db060458c13157b14287dab4534edf5
jrippy-cmd <jrippy@localhowl.com>
2025-09-16 12:13:26 -0500

    Update ascii-explosion.tsx

--- components/shared/effects/flame/ascii-explosion.tsx
+++ components/shared/effects/flame/ascii-explosion.tsx
@@ -39,7 +39,7 @@ export function AsciiExplosion(attrs: HTMLAttributes<HTMLDivElement>) {
       )}
     >
       <div
-        className="text-[#FA5D19] font-mono fc-decoration"
+        className="text-[rgb(8,101,252)] font-mono fc-decoration"
         dangerouslySetInnerHTML={{ __html: data[0] }}
         ref={ref}
         style={{
